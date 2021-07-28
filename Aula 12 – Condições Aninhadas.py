d3388b87e3ef357e7503e0a066339f230c113dd9
Diorgenes da Silva <65423187+DiorgenesSilva@users.noreply.github.com>
2021-07-28 16:28:27 -0300

    Condições Aninhadas.py
     Condições Aninhadas.py
    if :
    elif :
    else:

--- Aula 12 – Condições Aninhadas.py
+++ Aula 12 – Condições Aninhadas.py
@@ -1,5 +1,39 @@
-nome = str(input("Qual é o seu nome")).strip()
-if nome == "Gustavo"
-    print("Que bonito nome ")
-    print("tenha um bom dia {}".format(nome))  
+#CONDIÇÕES ANINHADAS
+print("""A estrutura condicional so tem 2 opçoes
+Ou é um caminho ou é outro
+Ja as condiçoes aninhadas voce tem
+uma gama muito maior de possibilidades
+if carroesquera:
+   carro.siga()
+   Carrodireita()
+   carro.siga()
+   carro.direita()
+   carro.esquerda9)
+   carro.siga()
+   carro.direita()
+   carro.siga()
+elif carro direita:
+   carro.siga()
+   carro.esquerda()
+   carro.siaga()
+   carro.esquerda()
+   carro.siga()
+slse carro.siga():
+   carro.siga()
+   
+""")
+#
+# Na Pratica
+#
+nome = str(input("Qual é o seu nome?"))
+if nome == Dio :
+   print("Bonito nome")
+elif == "Pedro" or == "Joao" or == 'Maris':
+   print("São nomes bem comum no Brasil") 
+else:
+   print("Seu nome é bem normal") 
+print("tenha um bom dia ").format(nome))
+
+
+ 
     
